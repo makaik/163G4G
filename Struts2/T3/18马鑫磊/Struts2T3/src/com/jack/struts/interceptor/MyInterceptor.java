2ee8fa2386d5a6fecb9262b5e5f8c4e05f657964
comjackjava <627470748@qq.com>
2017-11-29 16:56:55 +0800

    18马鑫磊提交Struts2T123章上机作业

---
+++ Struts2/T3/18马鑫磊/Struts2T3/src/com/jack/struts/interceptor/MyInterceptor.java
@@ -0,0 +1,19 @@
+package com.jack.struts.interceptor;
+
+import org.eclipse.jdt.internal.compiler.ast.Invocation;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class MyInterceptor extends AbstractInterceptor {
+
+	@Override
+	public String intercept(ActionInvocation arg0) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("我开始拦截");
+		String result =arg0.invoke();
+		System.out.println("返回代码"+result);
+		return result;
+	}
+
+}
